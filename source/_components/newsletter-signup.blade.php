93350e3c3c2837a007a31776ecec85f7828fa647
Hassan Raja <connect.rmhassan@gmail.com>
2021-03-26 11:34:38 +0500

    replace mailchimp subscribe action url

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-center lg:-mx-12 my-12 p-6 md:px-12 bg-gray-200 border border-gray-400 text-sm md:rounded shadow">
     <!-- Begin Mailchimp Signup Form -->
     <div id="mc_embed_signup">
-        <form action="https://your-mail-chimp-list-manage-url" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <form action="https://rmhassan.us1.list-manage.com/subscribe/post?u=2cc09a685a126a17b96083d3a&amp;id=73dccec1c7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
                 <h2>Sign up for our newsletter</h2>
                 <div class="mc-field-group">
@@ -27,3 +27,5 @@
         window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
     </script>
 @endpush
+
+
