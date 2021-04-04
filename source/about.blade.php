a4eebbbbced4bcab9a6ce94b0de8f42d66df9b95
Hassan Raja <connect.rmhassan@gmail.com>
2021-04-04 11:55:38 +0500

    update about

--- source/about.blade.php
+++ source/about.blade.php
@@ -16,4 +16,5 @@ description: A little bit about the site
     <p class="mb-6">This blog is a way to share where I'm at right now. That you are not alone where you are. We all start somewhere. It's true nobody knows everything.</p>
 
     <p class="mb-6">I work on Laravel applications, php, jquery, bootstrap. This is what I work on right now. I want to learn Vue, tailwind, typescript and what not.</p>
+    <p>Reach out to me <a href="https://twitter.com/RM_Hassan1">@RM_Hassan1</a>.</p>
 @endsection
