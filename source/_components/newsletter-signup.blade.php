cfc1950424d5677b5d0f56e36db1f3de35e0308a
Hassan Raja <connect.rmhassan@gmail.com>
2021-04-04 11:56:21 +0500

    update newsletter text

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -3,7 +3,7 @@
     <div id="mc_embed_signup">
         <form action="https://rmhassan.us1.list-manage.com/subscribe/post?u=2cc09a685a126a17b96083d3a&amp;id=73dccec1c7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-                <h2>Sign up for our newsletter</h2>
+                <h2>Sign up for monthly newsletter</h2>
                 <div class="mc-field-group">
                     <label for="mce-EMAIL">Email Address </label>
                     <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email address">
